46468a1967130ecbd7a0e3c42f0ed3a87340fef4
Deepak John <deepakjohn@outlook.com>
2018-05-04 15:41:46 +0100

    Los of chnages

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 let express = require('express');
 
 let app = express();
+const PORT = process.env.PORT || 5000;
 
 console.log('app started');
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app.use(express.static('.'));
+
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.sendFile('index.html');
 });
