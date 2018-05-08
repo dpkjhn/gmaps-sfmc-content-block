1934fd49b9e42b22dc417d07b4a1d76b4a32a637
Deepak John <deepakjohn@outlook.com>
2018-05-08 16:49:54 +0100

    Zoom parameter added

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## Introduction
 
+Proof of concept using Salesforce Marketing Cloud's Enhanced Content Block SDK.
+
+## Requirements
+
 [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
---
+++ mainApp.js
@@ -0,0 +1,64 @@
+// if (window.self === window.top) {
+//     document.body.innerText = 'This application is for use in the Salesforce Marketing Cloud Content Builder only!';;
+// }
+
+let sdk = new window.sfdc.BlockSDK(); //initalize SDK
+
+let mapData = {
+    apiKey: '',
+    mapCentre: 'London, UK',
+    mapHeight: 300,
+    mapWidth: 600,
+    mapZoom: 14,
+    mapMarker: {
+        color: '0xff0000',
+        label: 'London, UK'
+    }
+};
+
+let defaultContent = '<img width="600" src="https://maps.googleapis.com/maps/api/staticmap?center=London,+UK&zoom=12&scale=1&size=600x300&maptype=roadmap&format=png&visual_refresh=true" alt="Google Map">';
+
+let saveData = () => {
+    // console.log('Saving data...');
+
+    mapData.apiKey = document.getElementById('apiKey').value;
+    mapData.mapWidth = document.getElementById('mapWidth').value;
+    mapData.mapHeight = document.getElementById('mapHeight').value;
+    mapData.mapCentre = document.getElementById('mapCentre').value;
+    mapData.mapZoom = document.getElementById('mapZoom').value;
+
+
+    sdk.setData(mapData, (data) => {
+        // mapData = data;
+        let content = '<img width="100%" src="https://maps.googleapis.com/maps/api/staticmap?center=' + mapData.mapCentre + '&zoom=' + mapData.mapZoom + '&scale=1&size=' + mapData.mapWidth + 'x' + mapData.mapHeight + '&maptype=roadmap&format=png&visual_refresh=true&&markers=size:mid%7Ccolor:' + mapData.mapMarker.color + '%7Clabel:%7C' + mapData.mapCentre + '" alt="Google Map">';
+
+        sdk.setContent(content);
+    });
+
+    console.log(JSON.stringify(mapData));
+}
+
+let fetchData = () => {
+
+    // console.log('Loading data...');
+
+    sdk.getData((data) => {
+        if (Object.keys(data).length > 0) {
+            mapData = data;
+            document.getElementById('apiKey').value = mapData.apiKey;
+            document.getElementById('mapWidth').value = mapData.mapWidth;
+            document.getElementById('mapHeight').value = mapData.mapHeight;
+            document.getElementById('mapCentre').value = mapData.mapCentre;
+            // console.log('Found data!');
+        }
+    });
+
+    console.log(JSON.stringify(mapData));
+}
+
+sdk.setSuperContent(defaultContent, (newSuperContent) => {
+    defaultContent = newSuperContent;
+});
+
+window.onload = fetchData;
+window.onchange = saveData;
